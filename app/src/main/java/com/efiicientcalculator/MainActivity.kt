47d611feee7449212d2b7983d210f0cb75d50d2a
Sobhan.Ziyaei <sobhan.ziaee73@gmail.com>
2023-03-16 18:45:49 +0330

    complete app

--- app/src/main/java/com/efiicientcalculator/MainActivity.kt
+++ app/src/main/java/com/efiicientcalculator/MainActivity.kt
@@ -25,27 +25,36 @@ class MainActivity : AppCompatActivity() {
             "=" -> {
                 infixExpression = Expression(input)
                 resultTextBox?.text = infixExpression!!.evaluateExpression().toString()
+                input.clear()
+                input.add(resultTextBox?.text.toString())
             }
             "CL" -> {
                 input.clear()
                 resultTextBox?.text = ""
             }
             "C" -> {
-                resultTextBox?.text = "${resultTextBox?.text}".dropLast(2)
-                input.remove(input.last())
+                resultTextBox?.text = "${resultTextBox?.text}".dropLast(1)
+                if (resultTextBox?.text?.isNotEmpty()!! && resultTextBox?.text?.last() == ' ')
+                    resultTextBox?.text = "${resultTextBox?.text}".dropLast(1)
+
+                if (input.last().length == 1)
+                    input.removeAt(input.lastIndex)
+                else
+                    input[input.lastIndex] = input.last().dropLast(1)
+
             }
             else -> {
-                if (Character.isDigit(buttonText[0])) {
+                if (Character.isDigit(buttonText[0]) || buttonText[0] == '.') {
                     if (input.isNotEmpty() && Character.isDigit(input.last()[0])) {
                         input[input.lastIndex] = input.last() + buttonText
                         resultTextBox?.text = "${resultTextBox?.text}${button.text}"
                     } else {
                         input.add(buttonText)
-                        resultTextBox?.text = "${resultTextBox?.text}  ${button.text}"
+                        resultTextBox?.text = "${resultTextBox?.text} ${button.text}"
                     }
                 } else {
                     input.add(buttonText)
-                    resultTextBox?.text = "${resultTextBox?.text}  ${button.text}"
+                    resultTextBox?.text = "${resultTextBox?.text} ${button.text}"
                 }
 
 
